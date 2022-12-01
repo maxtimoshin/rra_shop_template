da4f5a1816725e54766d2cb01860d0e1bc668bd7
suffo99 <maxtimoshin94@gmail.com>
2022-12-01 17:29:24 +0200

    initiation

---
+++ precache-manifest.84610288e9c9757bd80b33885952138a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "41c0f8619aebb2dbf2a5",
+    "url": "./static/css/main.04315d91.chunk.css"
+  },
+  {
+    "revision": "41c0f8619aebb2dbf2a5",
+    "url": "./static/js/main.0e371abf.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "f594209432ee9bd8925b",
+    "url": "./static/css/2.b414f05d.chunk.css"
+  },
+  {
+    "revision": "f594209432ee9bd8925b",
+    "url": "./static/js/2.7b2f1aa3.chunk.js"
+  },
+  {
+    "revision": "9055ff8d18afe9d3dfd8ace338329323",
+    "url": "./static/media/logo.9055ff8d.svg"
+  },
+  {
+    "revision": "45d95a4bf4aa6f9b31dcf94d7b2adab8",
+    "url": "./static/media/cart-icon.45d95a4b.svg"
+  },
+  {
+    "revision": "86f202de58b8affd659839f743977499",
+    "url": "./index.html"
+  }
+];
